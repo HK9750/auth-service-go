c3398ce32e81c263841aae7b319c965c350b1883
HK9750 <hasnainkhan9750@gmail.com>
2026-03-08 22:56:10 +0500

    feat: started implementing my own rate limit middleware

--- internal/middleware/ratelimit_middleware.go
+++ internal/middleware/ratelimit_middleware.go
@@ -1,1 +1,29 @@
 package middleware
+
+import (
+	"github.com/redis/go-redis/v9"
+)
+
+type RateLimitConfig struct {
+	MaxLimit   int
+	WindowSize int
+}
+
+func NewRateLimitConfig(MaxLimit int, WindowSize int) *RateLimitConfig {
+	return &RateLimitConfig{
+		MaxLimit:   MaxLimit,
+		WindowSize: WindowSize,
+	}
+}
+
+type RateLimiter struct {
+	Client *redis.Client
+	Config *RateLimitConfig
+}
+
+func NewRateLimiter(Client *redis.Client, Config *RateLimitConfig) *RateLimiter {
+	return &RateLimiter{
+		Client: Client,
+		Config: Config,
+	}
+}
